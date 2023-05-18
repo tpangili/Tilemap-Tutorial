c134aaf8436464966777e8dea0ea1a05277ce90f
tpangili <tpangili@ucsc.edu>
2023-05-17 23:19:38 -0700

    first commit

---
+++ assets/tileset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.1" name="tileset" tilewidth="16" tileheight="16" tilecount="2112" columns="33">
+ <image source="tileset.png" width="528" height="1024"/>
+</tileset>
